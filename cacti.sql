6c2046ff6c199546ee32dfa5f4588e690fd24102
TheWitness <thewitness@cacti.net>
2024-12-15 11:36:09 -0500

    QA: Make GitHub Happy

--- cacti.sql
+++ cacti.sql
@@ -3194,8 +3194,8 @@ CREATE TABLE `sites` (
 -- Dumping data for table `sites`
 --
 
-INSERT INTO `sites` VALUES (1,'Edge','','','','','','','','',0.0000000000,0.0000000000,'','','', 0);
-INSERT INTO `sites` VALUES (2,'Core','','','','','','','','',0.0000000000,0.0000000000,'','','', 0);
+INSERT INTO `sites` VALUES (1,'Edge','','','','','','','','','',0.0000000000,0.0000000000,'','','', 0);
+INSERT INTO `sites` VALUES (2,'Core','','','','','','','','','',0.0000000000,0.0000000000,'','','', 0);
 
 --
 -- Table structure for table `snmpagent_cache`
